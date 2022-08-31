930d7ea135ca48980be204469ad9ade3bec634b2
valek97 <89108658715Fkmaf5>
2022-08-31 22:39:37 +0300

    Анимация и спрайты

--- Tunnel Rat/Assets/Scripts/Hero/Hero.cs
+++ Tunnel Rat/Assets/Scripts/Hero/Hero.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
-    [SerializeField]private float speed = 3f;
+    [SerializeField]private float speed = 5f;
     [SerializeField]private int lives = 5;
-    [SerializeField]private float jumpForce = 15f;
+    [SerializeField]private float jumpForce = 30f;
     private bool isGrounded = false;
 
     private Rigidbody2D rb;
+    private Animator anim;
     private SpriteRenderer sprite;
+
+    private States State
+    {
+        get { return (States)anim.GetInteger("state"); }
+        set { anim.SetInteger("state",(int)value); }
+    }
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        sprite = GetComponentInChildren<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
     private void FixedUpdate()
     {
@@ -22,25 +30,36 @@ public class Hero : MonoBehaviour
     }
     private void Update()
     {
+        if (isGrounded) State = States.idle;
         if (Input.GetButton("Horizontal")) Run();
         if (isGrounded && Input.GetButton("Jump")) Jump();
     }
     private void Run()
     {
+        if (isGrounded) State = States.run;
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
 
         transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
 
-        sprite.flipX = dir.x < 0.0f;
+        sprite.flipX = dir.x < 0.01f;
     }
     private void Jump()
     {
+        
         rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
     }
     private void Checkground()
     {
-        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.8f);
         isGrounded = collider.Length > 1;
+        if (!isGrounded) State = States.jump;
+        //Debug.Log($"collider.Length = "+ collider.Length);
     }
 
+}
+public enum States
+{
+    idle, 
+    run,
+    jump
 }
