fd2bd1214833ae2655b94ffbaa2525863b72ff5e
valek97 <kritenko97@gmail.com>
2023-01-09 00:22:28 +0300

    Корректировка скорости объекта, поиск до игрока при старте сцены.

--- Tunnel Rat/Assets/Scripts/Enemies/WalkingMonster.cs
+++ Tunnel Rat/Assets/Scripts/Enemies/WalkingMonster.cs
@@ -7,15 +7,16 @@ public class WalkingMonster : Entity
     //private float speed = 3.5f;
     private Vector3 dir;
     private SpriteRenderer sprite;
-    public Transform player;
+    private Transform player;
     private Rigidbody2D rb;
     private Vector3 movement;
-    public float moveSpeed = 5f;
+    public float moveSpeed = 300f;
 
     private void Start()
     {
         dir = transform.right;
         rb = this.GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Hero").transform;
     }
     private void Patrool()
     {
@@ -28,12 +29,12 @@ public class WalkingMonster : Entity
     }
     private void Move()
     {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position + transform.up * 0.1f + transform.right * dir.x * 0.7f, 0.1f);
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position + transform.up * 0.1f + transform.right * dir.x * 1.5f, 0.1f);
         /*if (colliders.Length > 0)
         {
             dir *= -1f;
         }*/
-        transform.position = Vector3.MoveTowards(transform.position, player.position, Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, player.position, (moveSpeed * Time.deltaTime));
     }
     /*private void TeleBomb()
     {
